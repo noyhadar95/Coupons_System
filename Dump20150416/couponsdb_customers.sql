32afc7b8519626e01ad83328bf1696e4b001ae9e
shush0606 <shush059@walla.com>
2015-04-16 22:30:57 +0300

    fixed sql files
    added USE couponsdb

--- Dump20150416/couponsdb_customers.sql
+++ Dump20150416/couponsdb_customers.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `customers`
 --
-
+USE couponsdb
 DROP TABLE IF EXISTS `customers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
