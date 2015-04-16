55754d9a5808ae0dce282d9d8f6ce7d9a479f6f6
Omer Hay <omerhay95@gmail.com>
2015-04-16 22:53:34 +0300

    added mysql jar

--- src/dal/DAL.java
+++ src/dal/DAL.java
@@ -17,7 +17,7 @@ public class DAL implements IDAL {
 
 	// Database credentials
 	static final String USER = "root";
-	static final String PASS = "root123";
+	static final String PASS = "";
 
 	public DAL() {
 		//connect();
