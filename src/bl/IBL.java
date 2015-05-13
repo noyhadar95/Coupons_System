6cd673edae9d6cde093ea3121a56d6ea1a09e08f
noyts <noyhadar95@gmail.com>
2015-05-14 00:41:52 +0300

    added some functions in sl and bl

--- src/bl/IBL.java
+++ src/bl/IBL.java
@@ -1,7 +1,17 @@
 package bl;
 
+import javax.swing.table.DefaultTableModel;
+
+import bl_backend.Coupon;
+
 public interface IBL {
 
 	boolean tryLogin(String username, String password, String authType);
+
+	boolean updateCouponRating(String couponName, int rating);
+
+	DefaultTableModel getCouponsDetails();
+
+	boolean updateCoupon(Coupon coupon);
 
 }
--- src/sl/SL.java
+++ src/sl/SL.java
@@ -1,7 +1,10 @@
 package sl;
 
+import javax.swing.table.DefaultTableModel;
+
 import bl.BL;
 import bl.IBL;
+import bl_backend.Coupon;
 
 public class SL implements ISL {
 
@@ -14,5 +17,26 @@ public class SL implements ISL {
 	@Override
 	public boolean tryLogin(String username, String password, String authType) {
 		return bl.tryLogin(username, password, authType);
+	}
+
+	@Override
+	public DefaultTableModel getCouponsNamesRatings(String customerName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateCouponRating(String couponName, int rating) {
+		return bl.updateCouponRating(couponName, rating);
+	}
+
+	@Override
+	public boolean updateCouponByAdmin(Coupon coupon) {
+		return bl.updateCoupon(coupon);
+	}
+
+	@Override
+	public DefaultTableModel getCouponsDetails() {
+		return bl.getCouponsDetails();
 	}
 }
