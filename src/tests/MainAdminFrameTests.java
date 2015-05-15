67896687f15032b97818f6582eedfab8824f7e0a
Omer Hay <omerhay95@gmail.com>
2015-05-15 16:51:55 +0300

    roll back

---
+++ src/tests/MainAdminFrameTests.java
@@ -0,0 +1,42 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainAdminFrameTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+	/*	Robot bot;
+		try {
+			bot = new Robot();
+		
+		bot.mouseMove(10,10);
+		bot.mousePress(InputEvent.BUTTON1_MASK);
+		//add time between press and release or the input event system may 
+		//not think it is a click
+		try{Thread.sleep(250);}catch(InterruptedException e){}
+		bot.mouseRelease(InputEvent.BUTTON1_MASK);
+		} catch (AWTException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}*/
+		fail("Not yet implemented"); // TODO
+	}
+
+}
