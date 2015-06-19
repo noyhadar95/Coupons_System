cea3a09f4d4796fa2a26ad5e6520a27e1608bd05
nimrodharris <harris_nimrod@hotmail.com>
2015-06-19 13:22:42 +0300

    Controller BuisnessOwner+Coupon
    Controllers

--- src/bl/CouponController.java
+++ src/bl/CouponController.java
@@ -1,5 +1,67 @@
 package bl;
 
-public class CouponController {
+import javax.swing.table.DefaultTableModel;
 
+import bl_backend.Coupon;
+import dal.DAL;
+import dal.IDAL;
+
+public class CouponController implements ICouponController {
+	private IDAL dal;
+
+	public CouponController() {
+		dal = new DAL();
+	}
+	
+	public DefaultTableModel getCouponsNamesRatings(String customerName) {
+		return dal.selectCouponsNamesRatingsByCustomer(customerName);
+	}
+	
+	@Override
+	public boolean updateCoupon(Coupon coupon) {
+		dal.updateCoupon(coupon);
+		return true;
+	}
+	
+	@Override
+	public DefaultTableModel getCouponsDetails() {
+		return dal.selectAllCoupons();
+	}
+	
+	@Override
+	public void insertCoupon(Coupon coupon) {
+		dal.insertCoupon(coupon);
+
+	}
+	
+	@Override
+	public int getNumOfUnapprovedCoupons() {
+		return dal.getNumOfUnapprovedCoupons();
+	}
+	@Override
+	public DefaultTableModel getApprovedCoupons() {
+		return dal.getApprovedCoupons();
+	}
+	
+	@Override
+	public DefaultTableModel getCouponsByFilter(String filter, String text) {
+		return dal.getCouponsByFilter(filter, text);
+	}
+	
+	@Override
+	public DefaultTableModel getCouponsByPreference(String customerName) {
+		return dal.getCouponsByPreference(customerName);
+	}
+	
+	@Override
+	public Coupon selectCoupon(String name) {
+		return dal.selectCoupon(name);
+	}
+	
+	@Override
+	public void deleteCoupon(String name) {
+		dal.deleteCoupon(name);
+		
+	}
+	
 }
